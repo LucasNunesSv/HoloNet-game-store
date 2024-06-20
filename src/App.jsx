3abb1d00cc1d7d8dfd3b2fe9a6c97c5636fdaab5
Lucas Nunes <lucasnsilveira202@gmail.com>
2024-06-20 10:20:32 -0300

    Implements addGame removeGame and handleSubmit functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(){
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  const [games, setGames] = useState([])
+  const [title, setTitle] = useState('')
+  const [coverImg, setCoverImg] = useState('')
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+
+    addGame({title, coverImg})
+
+    setTitle('')
+    setCoverImg('')
+  }
+
+  const addGame = ({title, coverImg}) => {
+    const id = Math.floor(Math.random() * 1000000)
+    const game = {id, title, coverImg}
+    setGames(state => [...state, game])
+  }
+
+  const removeGame = (id) => {
+    setGames(state => state.filter(game => game.id !== id ))
+  }
+
+  return(
+    <div className="app">
+      <h1>HoloNet Game Store</h1>
+      <form onSubmit={handleSubmit} action="">
+        <div>
+          <label htmlFor="title">Título</label>
+          <input 
+            type="text" 
+            name="title" 
+            id="title" 
+            value={title}
+            onChange={(ev) => setTitle(ev.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="coverImg">Imagem da Capa</label>
+          <input 
+            type="text" 
+            name="coverImg" 
+            id="coverImg" 
+            value={coverImg}
+            onChange={(ev) => setCoverImg(ev.target.value)}
+          />
+        </div>
+        <button type="submit">Adicionar a Biblioteca</button>
+      </form>
+      <div className="games">
+        {games.map((game) => (
+          <div key={game.id}>
+            <img src={game.coverImg} alt="" />
+            <div>
+              <h2>{game.title}</h2>
+              <button onClick={() => removeGame(game.id)}>Remover</button>
+            </div>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
-
-export default App
